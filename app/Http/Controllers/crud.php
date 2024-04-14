fb69b97d82187e5c408e700db05a3b237e58342e
AaronG19 <72134243+Aaron-Riley@users.noreply.github.com>
2024-04-14 21:12:36 +0100

    Set up environment and testing API's

---
+++ app/Http/Controllers/crud.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class crud extends Controller
+{
+    // 
+    function getData () {
+        return "Hello from getData";
+    }
+}
